20a0c35ba6d38fb0e035696857b0a7d8b4774893
Subhashreemuduli <subhashreemuduli097@gmail.com>
2022-09-13 23:43:31 +0530

    First commit

---
+++ src/MangoTree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MangoTree {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows");
+        int rows=sc.nextInt();
+        System.out.println("Enter number of columns");
+        int cols=sc.nextInt();
+        System.out.println("Enter tree number");
+        int num=sc.nextInt();
+
+
+
+
+
+    }
+}
---
+++ src/SwitchStatement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class SwitchStatement {
+    public static void main(String[] args)
+    {
+        System.out.println("Give your rating");
+        Scanner sc=new Scanner(System.in);
+        int rating=sc.nextInt();
+        switch(rating)
+        {
+            case 1:
+                System.out.println("One star rating");
+                break;
+            case 2:
+                System.out.println("Two star rating");
+                break;
+            case 3:
+                System.out.println("Three star rating");
+                break;
+            case 4:
+                System.out.println("Four star rating");
+                break;
+            case 5:
+                System.out.println("Five star rating");
+                break;
+            default:
+                System.out.println("Invalid rating");
+        }
+
+    }
+}
